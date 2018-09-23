421d8d2b20e360f84e29cb9c1d9d46727ce341a9
Unknown <diagarciaar@unal.edu.co>
2018-09-23 17:30:52 -0500

    mi avance de microservicio

--- Diego/snippets/serializers.py
+++ Diego/snippets/serializers.py
@@ -5,4 +5,4 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'service_id', 'service', 'comment', 'user_id','created_at')
