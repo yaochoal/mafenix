2cb9d32ce3f8c8596ab118161d0f645268cd257f
idrojasf <idrojasf@unal.edu.co>
2018-10-03 23:17:35 -0500

    Add some validations

--- Ivan/users-restapi/models/user.go
+++ Ivan/users-restapi/models/user.go
@@ -10,3 +10,16 @@ type User struct {
 	Avatar   string        `bson:"avatar" json:"avatar"`
 	Password string        `bson:"password" json:"password"`
 }
+
+
+func (c Customer) Validate() error {
+	return validation.ValidateStruct(&c,
+		// Name cannot be empty, and the length must be between 5 and 20.
+		validation.Field(&c.Ṕassword, validation.Required, validation.Length(5, 20)),
+		// Gender is optional, and should be either "Female" or "Male".
+		validation.Field(&c.Name, validation.Required, is.Name),
+		// Validate Address using its own validation rules
+		validation.Field(&c.Email, validation.Required, is.Email),
+		// Validate Address using its own validation rules
+	)
+}
