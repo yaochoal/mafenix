0e8cfbe72736064337552b64c19cf21d0f55e280
Yesid Oc <yaochoal@unal.edu.co>
2018-10-06 19:52:43 -0500

    Actualizacion

--- scoreresources-ms/api/controllers/todoListController.js
+++ scoreresources-ms/api/controllers/todoListController.js
@@ -14,12 +14,23 @@ exports.list_all_tasks = function(req, res) {
   });
 };
 exports.search_tasks = function(req, res) {
-
-  console.log(req.body.service)
+  //console.log(req.body.service)
   Task.find({'service':req.body.service,'service_id':req.body.service_id}, function(err, task) {
     if (err)
       res.send(err);
-    res.json(task);
+    //res.json(task);
+    var Objeto = {};
+    Objeto["excelente"]= task.filter(function(item){return (item.score==5)}).length;
+    Objeto["bueno"]= task.filter(function(item){return (item.score==4)}).length;
+    Objeto["medio"]= task.filter(function(item){return (item.score==3)}).length;
+    Objeto["regular"]= task.filter(function(item){return (item.score==2)}).length;
+    Objeto["malo"]= task.filter(function(item){return (item.score==1)}).length;
+    res.json(Objeto);
+    //console.log(task.filter(function(item){return (item.score==5)}).length)
+    //console.log(task.filter(function(item){return (item.score==4)}).length)
+    //console.log(task.filter(function(item){return (item.score==3)}).length)
+    //console.log(task.filter(function(item){return (item.score==2)}).length)
+    //console.log(task.filter(function(item){return (item.score==1)}).length)
   });
 };
 
