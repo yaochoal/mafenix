673f2d62d05d18ea8b5c7a2f5652e67c75612990
Yesid Oc <yaochoal@unal.edu.co>
2018-10-21 00:18:27 -0500

    Visualizacion de recursos materias y docentes en movil

--- mafenix-ma/src/pages/Courses.js
+++ mafenix-ma/src/pages/Courses.js
@@ -1,26 +1,142 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, Picker} from 'react-native';
-import { DrawerNavigator, StackNavigator, DrawerItems, SafeAreaView } from 'react-navigation'
-
+import image from "./book.png"
+import {client} from '../utils/graphQLUtils';
+import gql from "graphql-tag";
+import { View, FlatList, ActivityIndicator } from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
 
 export default class Courses extends React.Component {
-    render() {
-        return (
-          <View style={styles.container}>
-          <Text>Bienvenido a las materias</Text>
-          </View>
-          
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      error: null,
+      refreshing: false
+    };
+  }
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    if(this.state.page <= 5){
+    const { page } = this.state;
+    this.setState({ loading: true });
+      client.query({
+        query: gql`query{
+          allCourses(page:${page}){
+            id
+            name
+            description
+            code
+          }
+        }
+        `
+      }).then(res => {
+               ///codigo
+               console.log(res.data.allCourses);
+               this.setState({
+                data: page === 1 ? res.data.allCourses : [...this.state.data, ...res.data.allCourses],
+                error: res.error || null,
+                loading: false,
+                refreshing: false
+              });
+      })
+      .catch(error => {
+          console.log(error);
+          this.setState({ error, loading: false });
+        });
       }
-    
+  };
+
+
+  handleRefresh = () => {
+    if(this.state.page <= 5){
+    this.setState(
+      {
+        page: 1,
+        seed: this.state.seed + 1,
+        refreshing: true
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  }
+  };
+
+  handleLoadMore = () => {
+    if(this.state.page <= 5){
+    this.setState(
+      {
+        page: this.state.page + 1
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+    }
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return <SearchBar placeholder="Buscar..." lightTheme round />;
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              roundAvatar
+              title={`${item.name} ${item.code}`}
+              subtitle={item.description}
+              avatar={ image}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => `${item.id}`}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={50}
+        />
+      </List>
+    );
+  }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- mafenix-ma/src/pages/Teachers.js
+++ mafenix-ma/src/pages/Teachers.js
@@ -1,26 +1,141 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, Picker} from 'react-native';
-import { DrawerNavigator, StackNavigator, DrawerItems, SafeAreaView } from 'react-navigation'
+import image from "./perfil.jpg"
+import {client} from '../utils/graphQLUtils';
+import gql from "graphql-tag";
+import { View, FlatList, ActivityIndicator } from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
 
+export default class Teachers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      error: null,
+      refreshing: false
+    };
+  }
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
 
-export default class Home extends React.Component {
-    render() {
-        return (
-          <View style={styles.container}>
-          <Text>Bienvenido a los profesores</Text>
-          </View>
-          
-        );
+  makeRemoteRequest = () => {
+    if(this.state.page <= 5){
+    const { page } = this.state;
+    this.setState({ loading: true });
+      client.query({
+        query: gql`query{
+          allTeachers(page:${page}){
+            id
+            name
+            description
+          }
+        }
+        `
+      }).then(res => {
+               ///codigo
+               console.log(res.data.allTeachers);
+               this.setState({
+                data: page === 1 ? res.data.allTeachers : [...this.state.data, ...res.data.allTeachers],
+                error: res.error || null,
+                loading: false,
+                refreshing: false
+              });
+      })
+      .catch(error => {
+          console.log(error);
+          this.setState({ error, loading: false });
+        });
       }
-    
+  };
+
+
+  handleRefresh = () => {
+    if(this.state.page <= 5){
+    this.setState(
+      {
+        page: 1,
+        seed: this.state.seed + 1,
+        refreshing: true
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  }
+  };
+
+  handleLoadMore = () => {
+    if(this.state.page <= 5){
+    this.setState(
+      {
+        page: this.state.page + 1
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+    }
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return <SearchBar placeholder="Buscar..." lightTheme round />;
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              roundAvatar
+              title={`${item.name} `}
+              subtitle={item.description}
+              avatar={ image}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => `${item.id}`}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={50}
+        />
+      </List>
+    );
+  }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
