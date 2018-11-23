61dcb6b97322a15adf90722f10a8f799f236e6fb
Yesid Oc <yaochoal@unal.edu.co>
2018-11-23 17:38:44 -0500

    Actualización entrega 4

--- resources-ms/myapp/config/routes.rb
+++ resources-ms/myapp/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :resources1
   resources :teachers1
   resources :courses1
+  wash_out :wsresources
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
